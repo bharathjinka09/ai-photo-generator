9e7fb33b626afccc24abd4b25a60da4336767930
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-23 15:57:09 +0530

    Updates

---
+++ precache-manifest.eed3cea1127c86024373fdd67c4ab26c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d720ebbc349c1941e48f09d11b391017",
+    "url": "/ai-photo-generator/index.html"
+  },
+  {
+    "revision": "26e82db4556a6109fccb",
+    "url": "/ai-photo-generator/static/css/main.bdce3666.chunk.css"
+  },
+  {
+    "revision": "433c66894642d8329a96",
+    "url": "/ai-photo-generator/static/js/2.10482ee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ai-photo-generator/static/js/2.10482ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e82db4556a6109fccb",
+    "url": "/ai-photo-generator/static/js/main.aee3a838.chunk.js"
+  },
+  {
+    "revision": "78e64bd843a6627dff4b",
+    "url": "/ai-photo-generator/static/js/runtime-main.e9b55974.js"
+  }
+]);
